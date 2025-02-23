5555a367978f4a7dbf0c63f317cb55cf35144ae6
iamyosuke <murata@open-dna.jp>
2025-02-23 18:31:52 +0900

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['ukwsebzgjwpsjkepppac.supabase.co'],
+  },
 };
 
 export default nextConfig;
